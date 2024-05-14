97e56b368b54912fb1336778818a979425f6e6f1
waleed <120984229+waleed12121212@users.noreply.github.com>
2024-05-14 20:23:18 +0300

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCrud.Data;
+using StudentCrud.Models;
+using StudentCrud.Data;
+namespace StudentCrud.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDbContext context = new StudentDbContext();
+        public IActionResult Index( )
+        {
+            var students = context.Students.ToList();
+            return View("Index" , students);
+        }
+        public IActionResult Details(int id)
+        {
+            var student = context.Students.Find(id);
+            return View("Details" , student);
+        }
+        public IActionResult Create( )
+        {
+            return View("Create");
+        }
+        public IActionResult Store(Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = context.Students.Find(id);
+            context.Students.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = context.Students.Find(id);
+            return View("Edit" , student);
+        }
+        public IActionResult Update(Student student)
+        {
+            var result = context.Students.Find(student.Id);
+            // context.Students.Update(student);
+            result.Name = student.Name;
+            result.Age = student.Age;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCrud.Models;
+using System.Collections.Generic;
+
+namespace StudentCrud.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=db5019.public.databaseasp.net; Database=db5019; User Id=db5019; Password=X?o68aP!=L5i; Encrypt=False; MultipleActiveResultSets=True;");
+        }
+    }
+}
